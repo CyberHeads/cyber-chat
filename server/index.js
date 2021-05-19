5b1fa2121a836b08cec2d1ee912d5373e237c454
Minura_Kariyawasam <minura.kariyawasam@arimaclanka.com>
2021-05-19 19:40:52 +0530

    update join page

--- server/index.js
+++ server/index.js
@@ -3,8 +3,21 @@ const socketio = require('socket.io');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router');
+const { Socket } = require('dgram');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connect', (socket) => {
+    console.log('we have a new connection !!');
+
+    socket.on('disconnect', () => {
+        console.log('user hav left!!')
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
