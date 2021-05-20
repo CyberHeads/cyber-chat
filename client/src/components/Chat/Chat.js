4b7bdcec9dc8f6d57c7fd3649f3acc67d6e66d39
Adee99 <66461199+Adee99@users.noreply.github.com>
2021-05-20 20:22:39 +0530

    update : chat option :+1:

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
  const Chat = (location) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessages] = useState([]);
+    //const [messages, setMessages] = useState([]);
 
     const ENDPOINT = 'localhost:5000'; 
 
@@ -18,9 +20,23 @@ let socket;
         setName(name);
         setRoom(room);
 
-        console.log(socket);
 
+       socket.emit('join',{name, room}, () =>{
+
+       });
+       return ()=>{
+           socket.emit('disconnect');
+           
+           socket.off();
+       }
      }, [ENDPOINT, location.search]);
+
+     useEffect(() => {
+         socket.on('message',(message) => {
+
+         })
+     })
+
     return (
         <h1>Chat</h1>
     )
